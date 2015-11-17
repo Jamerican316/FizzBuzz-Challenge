22f65aeb85b097dad0a95ccfee09803f5838549e
Jamerican316 <karanja.broomfield@gmail.com>
2015-11-17 12:29:55 -0800

    refactored FizzBuzz

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
-    	for( var i=1; i<=100; i++ ) {
-
-        if( (i%3) === 0 && (i%5) === 0 ) {
-            $('body').append('FizzBuzz');
-        }else if( (i%3) === 0 ) {
-            $('body').append('<div>Fizz</div>');
-        }else if( (i%5) === 0 ) {
-            $('body').append('<div>Buzz</div>');
-        }else{
-            $('body').append('<div>'+i+'</div>');
-        }
+      var userNumber = prompt("Please pick a number...");
+      +userNumber;
+    for( var i=1; i<=userNumber; i++ ) {
+            if( (i%3) === 0 && (i%5) === 0 ) {
+                    $('body').append('FizzBuzz');
+            }else if( (i%3) === 0 ) {
+                    $('body').append('<div>Fizz</div>');
+            }else if( (i%5) === 0 ) {
+                    $('body').append('<div>Buzz</div>');
+            }else{
+                    $('body').append('<div>'+i+'</div>');
+            }
     }
 });
